b70aed7d609ffac3c3b5e8a657f1895aa6d8b8bc
Kostek Sytnyk <ksytnik73kwer@gmail.com>
2025-02-17 14:17:05 +0000

    Add a part of necessary components, add comment for Home Page

--- personal-website/src/app/page.tsx
+++ personal-website/src/app/page.tsx
@@ -2,7 +2,14 @@ export default function Home() {
   return (
     <div className="">
       <main className="">
-        
+        {/* 
+        Hero section:
+        Name, short bio: left side
+        Image: right side
+
+        Featured blogs section (2x1 grid)
+        Skill set section - grid of cards. Each card includes info: “icon”, “title”
+        */}
       </main>
     </div>
   );
