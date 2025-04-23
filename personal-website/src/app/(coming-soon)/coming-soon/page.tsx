f9de0a9a96184bc36314f70b4891f331070872ed
Kostiantyn <ksytnik73kwer@gmail.com>
2025-04-23 19:16:25 +0200

    Add ReturnHome Button

--- personal-website/src/app/(coming-soon)/coming-soon/page.tsx
+++ personal-website/src/app/(coming-soon)/coming-soon/page.tsx
@@ -1,12 +1,23 @@
-import styles from "@/styles/comingSoon.module.css";
+"use client";
+import styles from '@/styles/comingSoon.module.css';
+import { useRouter } from 'next/navigation';
 
 export default function ComingSoon() {
+  const router = useRouter();
+
+  const handleReturnHome = () => {
+    router.push("/");
+  };
+
   return (
     <div className={styles.mainContainer}>
       <p className={styles.headerText}>Coming Soon...</p>
       <p className={styles.subheaderText}>
         Hard work happens behind the scenes to bring you all great experience!
       </p>
+      <button onClick={handleReturnHome} className={styles.returnHomeButton}>
+        Return Home
+      </button>
     </div>
   );
 }
